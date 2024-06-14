894dde68665a0e472ab57a9594b2356f84c7eb5e
jayhklasiq <joshvolx@gmail.com>
2024-06-14 14:21:41 +0000

    updated callback link

--- utilities/auth.js
+++ utilities/auth.js
@@ -9,7 +9,7 @@ const connectDB = require('../data/connect');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5500/auth/google/callback",
+  callbackURL: "https://diagnosis-api-gk2d.onrender.com/auth/google/callback",
   passReqToCallback: true
 },
   async function (request, accessToken, refreshToken, profile, done) {
